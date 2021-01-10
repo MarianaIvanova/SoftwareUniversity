4f34da7aa5582331301111bd74cff587f4a9ebc1
Mariana Ivanova <mariivanova@yahoo.com>
2021-01-10 23:02:19 +0200

    All files from topic 8.Generics from lab (1 to 3) added

---
+++ CSharp-Advanced/8.Generics/Lab 1 BoxOfT/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private Stack<T> internalStack;
+        public Box()
+        {
+            internalStack = new Stack<T>();
+        }
+        public int Count 
+        { 
+            get
+            {
+                return internalStack.Count;
+            }
+        }
+
+        public void Add(T element)
+        {
+            internalStack.Push(element);
+        }
+
+        public T Remove()//RemoveTopmostElement()
+        {
+            if(internalStack.Count == 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return internalStack.Pop();
+        }
+    }
+}
---
+++ CSharp-Advanced/8.Generics/Lab 2 Array Creator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+            int[] integers = ArrayCreator.Create(10, 33);
+            //Console.WriteLine(String.Join(", ", strings));//Pesho, Pesho, Pesho, Pesho, Pesho
+            //Console.WriteLine(String.Join(", ", integers));//33, 33, 33, 33, 33, 33, 33, 33, 33, 33
+        }
+    }
+}
