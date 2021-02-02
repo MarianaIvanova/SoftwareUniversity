d2beec03cad3e651b009de7b296aae3cf8c98e18
Mariana Ivanova <mariivanova@yahoo.com>
2021-02-02 18:21:38 +0200

    Update 04. Built-In-Functions from MS SQL

--- MS SQL/04. Built-In-Functions.sql
+++ MS SQL/04. Built-In-Functions.sql
@@ -382,7 +382,16 @@ SELECT RANK() OVER (ORDER BY Salary DESC), Salary, FirstName, LastName, JobTitle
 
 --DENSE_RANK – returns the same rank for ties, but it doesn’t    have any gaps in the sequence
 --The numbering of the ordered list is done like this, that if there are people with one and the same salary they will be one and the same rank(one is 6-th and the other is 6-th for example too, both have 50500 and there is 7-th rank in the list)
-SELECT DENSE_RANK() OVER (ORDER BY Salary DESC), Salary, FirstName, LastName, JobTitle
+--DENSE_RANK without PARTITION
+SELECT 
+		DENSE_RANK() OVER (ORDER BY Salary DESC) AS DENSE_RANK_Simple, 
+		Salary, FirstName, LastName, JobTitle
+	 FROM [SoftUni].[dbo].[Employees]
+	 ORDER BY Salary DESC
+--DENSE_RANK with PARTITION
+SELECT 
+		DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID)  AS DENSE_RANK_With_Partition,
+		EmployeeID, Salary, FirstName, LastName, JobTitle
 	 FROM [SoftUni].[dbo].[Employees]
 	 ORDER BY Salary DESC
 
@@ -437,52 +446,53 @@ SELECT *
 	 WHERE JobTitle LIKE '[^EMA]%Man_ger' ESCAPE '_' -- here '_' is used as symbol
 
 
---Exercises
---Queries for SoftUni Database
+--Exercises - IN JUDGE - TABLES IN THE QUERIES SHOULD BE ONLY WITH THEIR NAMES, NOT WITH DATABASE AND SCHEMAS  
+
+--Part I – Queries for SoftUni Database
 --Ex 1. Find Names of All Employees by First Name
 --Write a SQL query to find first and last names of all employees whose first name starts with "SA". 
 USE SoftUni
 SELECT FirstName, LastName 
-	FROM Employees
+	FROM [SoftUni].[dbo].[Employees]
 	WHERE LEFT(FirstName,2) = 'SA'
 
 --Ex 2. Find Names of All employees by Last Name 
 --Write a SQL query to find first and last names of all employees whose last name contains "ei".
 SELECT FirstName, LastName 
-	FROM Employees
+	FROM [SoftUni].[dbo].[Employees]
 	WHERE LastName LIKE '%ei%'
 
 --Ex 3. Find First Names of All Employees
 --Write a SQL query to find the first names of all employees in the departments with ID 3 or 10 and whose hire year is between 1995 and 2005 inclusive.
 SELECT FirstName
-	FROM Employees
+	FROM [SoftUni].[dbo].[Employees]
 	WHERE DepartmentID IN (3, 10) 
 		AND DATEPART(YEAR,HireDate) BETWEEN 1995 AND 2005
 
 --Ex 4. Find All Employees Except Engineers
 --Write a SQL query to find the first and last names of all employees whose job titles does not contain "engineer". 
 SELECT FirstName, LastName
-	FROM Employees
+	FROM [SoftUni].[dbo].[Employees]
 	WHERE JobTitle NOT LIKE '%engineer%' 
 
 --Ex 5. Find Towns with Name Length
---Write a SQL query to find town names that are 5 or 6 symbols long and order them alphabetically by town name. 
+--Write a SQL query to find town names that are 5 or 6 symbols long and order them alphabetically by town name. --
 SELECT [Name]
-	FROM Towns
+	FROM [SoftUni].[dbo].[Towns]
 	WHERE LEN([Name]) IN (5,6)
 	ORDER BY [Name]
 
 --Ex 6. Find Towns Starting With
 --Write a SQL query to find all towns that start with letters M, K, B or E. Order them alphabetically by town name. 
 SELECT TownID, [Name]
-	FROM Towns
+	FROM [SoftUni].[dbo].[Towns]
 	WHERE LEFT([Name],1) IN ('M','K','B','E')
 	ORDER BY [Name]
 	
 --Ex 7. Find Towns Not Starting With
 --Write a SQL query to find all towns that does not start with letters R, B or D. Order them alphabetically by name. 
 SELECT TownID, [Name]
-	FROM Towns
+	FROM [SoftUni].[dbo].[Towns]
 	WHERE LEFT([Name],1) NOT IN ('R','B','D')
 	ORDER BY [Name]	
 
@@ -490,17 +500,108 @@ SELECT TownID, [Name]
 --Write a SQL query to create view V_EmployeesHiredAfter2000 with first and last name to all employees hired after 2000 year.
 CREATE VIEW V_EmployeesHiredAfter2000 AS
 SELECT FirstName, LastName
-	FROM Employees
+	FROM [SoftUni].[dbo].[Employees]
 	WHERE DATEPART(YEAR, HireDate) > 2000
 
 --Ex 9. Length of Last Name
 --Write a SQL query to find the names of all employees whose last name is exactly 5 characters long.
 SELECT FirstName, LastName
-	FROM Employees
+	FROM [SoftUni].[dbo].[Employees]
 	WHERE LEN(LastName) = 5
 
 --Ex 10. Rank Employees by Salary
 --Write a query that ranks all employees using DENSE_RANK. In the DENSE_RANK function, employees need to be partitioned by Salary and ordered by EmployeeID. You need to find only the employees whose Salary is between 10000 and 50000 and order them by Salary in descending order.
+SELECT EmployeeID, FirstName, LastName, Salary,
+		DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [RANK] 
+	FROM [SoftUni].[dbo].[Employees]
+	WHERE Salary BETWEEN 10000 and 50000
+	ORDER BY Salary DESC
+
+--Ex 11. Find All Employees with Rank 2 *
+--Use the query from the previous problem and upgrade it, so that it finds only the employees whose Rank is 2 and again, order them by Salary (descending).
+
+WITH NewShortTableEmployees AS
+(SELECT TOP(293) EmployeeID, FirstName, LastName, Salary,
+		DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS [RANK] 
+	FROM [SoftUni].[dbo].[Employees]
+	WHERE Salary BETWEEN 10000 and 50000
+	ORDER BY Salary DESC)
 SELECT *
-	FROM Employees
-	WHERE 
+	FROM NewShortTableEmployees
+	WHERE [RANK] = 2
+	ORDER BY Salary DESC
+
+--Part II – Queries for Geography Database 
+
+--Ex 12. Countries Holding ‘A’ 3 or More Times
+--Find all countries that holds the letter 'A' in their name at least 3 times (case insensitively), sorted by ISO code. Display the country name and ISO code. 
+--USE Geography
+SELECT CountryName, IsoCode
+  FROM [Geography].[dbo].[Countries]
+  WHERE CountryName LIKE '%A%A%A%'
+  ORDER BY IsoCode
+
+--Ex 13. Mix of Peak and River Names
+--Combine all peak names with all river names, so that the last letter of each peak name is the same as the first letter of its corresponding river name. Display the peak names, river names, and the obtained mix (mix should be in lowercase). Sort the results by the obtained mix.
+SELECT p.PeakName, r.RiverName, 
+		LOWER(CONCAT(p.PeakName, SUBSTRING(r.RiverName,2, LEN(r.RiverName) -1))) AS Mix
+	FROM [Geography].[dbo].[Peaks] AS p
+	JOIN [Geography].[dbo].[Rivers] AS r ON RIGHT(p.PeakName,1) = LEFT(r.RiverName,1)
+	ORDER BY Mix
+
+--Part III – Queries for Diablo Database
+
+--Ex 14. Games from 2011 and 2012 year
+--Find the top 50 games ordered by start date, then by name of the game. Display only games from 2011 and 2012 year. Display start date in the format "yyyy-MM-dd". 
+SELECT TOP (50) [Name], FORMAT([Start], 'yyyy-MM-dd', 'bg-BG') AS [Start]
+  FROM [Diablo].[dbo].[Games]
+  WHERE DATEPART(YEAR,[Start]) IN (2011,2012)
+  ORDER BY [Start], [Name]
+
+--Ex 15. User Email Providers
+--Find all users along with information about their email providers. Display the username and email provider. Sort the results by email provider alphabetically, then by username. 
+SELECT Username, 
+	SUBSTRING(Email, CHARINDEX ('@', Email, 1) + 1, LEN(Email) - CHARINDEX ('@', Email, 1)) 
+	AS [Email Provider]
+  FROM [Diablo].[dbo].[Users]
+  ORDER BY [Email Provider], Username
+
+--Ex 16. Get Users with IPAdress Like Pattern
+--Find all users along with their IP addresses sorted by username alphabetically. Display only rows that IP address matches the pattern: "***.1^.^.***".
+--Legend: * - one symbol, ^ - one or more symbols
+SELECT Username, IpAddress
+  FROM [Diablo].[dbo].[Users]
+  WHERE IpAddress LIKE '___.1%.%.___'
+  ORDER BY Username
+
+--Ex 17. Show All Games with Duration and Part of the Day
+--Find all games with part of the day and duration sorted by game name alphabetically then by duration (alphabetically, not by the timespan) and part of the day (all ascending). Parts of the day should be Morning (time is >= 0 and < 12), Afternoon (time is >= 12 and < 18), Evening (time is >= 18 and < 24). Duration should be Extra Short (smaller or equal to 3), Short (between 4 and 6 including), Long (greater than 6) and Extra Long (without duration). 
+SELECT [Name],
+	CASE 
+		WHEN (DATEPART(Hour, Start) * 60 * 60  + DATEPART(Minute,Start) * 60 + DATEPART(Second, Start) >= 0 AND  DATEPART(Hour, Start) * 60 * 60  + DATEPART(Minute,Start) * 60 + DATEPART(Second, Start) < 12 * 60 * 60)
+			THEN 'Morning'
+		WHEN (DATEPART(Hour, Start) * 60 * 60  + DATEPART(Minute,Start) * 60 + DATEPART(Second, Start) >= 12 * 60 * 60 AND  DATEPART(Hour, Start) * 60 * 60  + DATEPART(Minute,Start) * 60 + DATEPART(Second, Start) < 18 *	 60 * 60)
+			THEN 'Afternoon'
+		WHEN (DATEPART(Hour, Start) * 60 * 60  + DATEPART(Minute,Start) * 60 + DATEPART(Second, Start) >= 18 * 60 * 60 AND  DATEPART(Hour, Start) * 60 * 60  + DATEPART(Minute,Start) * 60 + DATEPART(Second, Start) < 24 *	 60 * 60)
+			THEN 'Evening'
+	END AS [Part of the Day],
+	CASE
+		WHEN Duration IS NULL THEN 'Extra Long'
+		ELSE
+			CASE
+				WHEN Duration <= 3 THEN 'Extra Short'
+				WHEN Duration >= 4 AND Duration <= 6 THEN 'Short'
+				WHEN Duration > 6 THEN 'Long'
+			END 
+	END AS Duration
+  FROM [Diablo].[dbo].[Games]
+  ORDER BY [Name] ASC, Duration ASC, [Part of the Day] ASC
+
+--Part IV – Date Functions Queries
+
+--Ex 18. Orders Table
+--You are given a table Orders(Id, ProductName, OrderDate) filled with data. Consider that the payment for that order must be accomplished within 3 days after the order date. Also the delivery date is up to 1 month. Write a query to show each product’s name, order date, pay and deliver due dates. 
+SELECT ProductName, OrderDate,
+	DATEADD(DAY, 3, OrderDate) AS [Pay Due], 
+	DATEADD(MONTH, 1, OrderDate) AS [Deliver Due]
+	FROM [Orders].[dbo].[Orders]
